3913ecb117a2b7fee96fdd4becde0aac8ab156f9
diogo <diogo@localhost.localdomain>
2017-12-18 13:18:29 +0000

    api from server done

--- routes/newsapi.js
+++ routes/newsapi.js
@@ -1,24 +1,37 @@
 //const url = require('url');
 const NewsAPI = require('newsapi');
-
+const url = require('url')
+const querystring = require('querystring');
 /* GET home page. */
 var apiKey = '16bb1647c69145dc8a5c8f18373cd2cf'
 const Newsapi = new NewsAPI(apiKey);
 
+var o = Object.create(Object.prototype, {
+  topHeadlines: {
+    writable: false,
+    configurable: false,
+    value: function (req, res) {
+      Newsapi.v2.topHeadlines(req).then((response) => {
+        res.send(response)
+      });
+    }
+  },
+  everything: {
+    writable: false,
+    configurable: false,
+    value: function (req, res) {
+      Newsapi.v2.topHeadlines(req).then((response) => {
+        res.send(response)
+      });
+    }
+  }
+});
 function newsapi(req, res) {
-  /* var search = url.parse(req.url).search
-   var pathname = url.parse(req.url).pathname.split('/').pop()
-   let lo = `/v2/${pathname}${search}&apiKey=${apiKey}`
-   var option = url.resolve(`${pathname}`, search)*/
-  Newsapi.v2.topHeadlines({
-    sources: '',
-    q: 'trump',
-    category: '',
-    language: 'en',
-    country: ''
-  }).then((response) => {
-    console.log(res, response);
-  });
+  const reqObj = querystring.parse(url.parse(req.url).query, null, null)
+  var pathname = url.parse(req.url).pathname.split('/').pop()
+  if (o.hasOwnProperty(pathname)) {
+    o[pathname](reqObj, res)
+  }
 }
 
 module.exports = newsapi;
--- routes/youtube.js
+++ routes/youtube.js
@@ -1,26 +1,21 @@
-const https = require('https')
-const fs = require('fs')
-const { URLSearchParams } = require('url');
-const url = require('url');
-const path = require('path')
+const url = require('url')
+const querystring = require('querystring');
+var youtubeNode = require("youtube-node")
+var youTubesearch = new youtubeNode();
 /* GET home page. */
 var key = 'AIzaSyAvZS0WP2y0M9Yczpi8F-4JeagLnFCDMwU'
 
 function youtube(req, res) {
-  var search = url.parse(req.url).search
-  var pathname = url.parse(req.url).pathname.split('/').pop()
-  let lo = search + `&key=${key}`
-  var option = url.resolve(`youtube/v3/${pathname}`, lo)
-  var options = {
-    hostname: "www.googleapis.com",
-    path: option,
-    port: 443,
-    method: 'GET'
-  }
-  console.log('youtube', option)
-  let requ = https.request(options, function (reqRes) {
-    console.log('youtube', reqRes)
-  })
+  const myURL = querystring.parse(url.parse(req.url).query, null, null)
+  var pathname = url.parse(req.url).pathname.split('/').pop()  
+  youTubesearch.setKey(key);
+  youTubesearch.search(myURL.q, myURL.maxResults, function (error, result) {
+    if (error) {
+      console.log(error);
+    }
+    else {      
+      res.send(result)
+    }
+  });
 }
-
 module.exports = youtube;
