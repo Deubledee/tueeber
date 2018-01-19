d146ccca22ecbf60f99a277f6fb6d8379a84ba58
diogo <diogo@localhost.localdomain>
2018-01-19 14:23:02 +0000

    transfeeeerrrrrrrr

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const expressSession = require("express-session")
+var RedisStore = require('connect-redis')(expressSession);
+const redis = require('redis');
+//const RedisServer = require('redis-server');
+var commands = require('redis-commands');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
@@ -9,9 +14,6 @@ const youtube = require('./routes/youtube');
 const newsapi = require('./routes/newsapi');
 const app = express();
 const responseTime = require('response-time')
-const redis = require('redis');
-//const RedisServer = require('redis-server');
-var commands = require('redis-commands');
 /*const server = new RedisServer({
   port: 6379,
   conf: 'redis.conf'
@@ -30,9 +32,10 @@ var obj = {
     // server bound to `server.port` (e.g. 6379).
     console.log('server open')
   }
-});
-**/
-const client = redis.createClient();
+});*/
+
+const client = redis.createClient(process.env.REDIS_URL)
+//const client = redis.createClient()
 client.on('connect', function (item) {
   console.log('redis conected port: 6379')
 })
@@ -45,6 +48,74 @@ commands.list.forEach(function (command) {
   console.log(command);
 });
 */
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser('malaguenhaaaaa'));
+app.use(responseTime());
+app.use('/publicbower', express.static(path.join(__dirname, 'build/modern/public/bower_components/webcomponentsjs')));
+app.use('/public', express.static(path.join(__dirname, 'build/modern/public')));
+app.use('/src', express.static(path.join(__dirname, 'build/modern/src')));
+app.use('/publicbower', express.static(path.join(__dirname, 'public/bower_components/webcomponentsjs')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+
+app.use(function (req, res, next) {
+  if (req.session) {
+    return next();
+  }
+
+  var tryCount = 1,
+    maxTries = 3;
+
+  function lookupSession(error) {
+    var sessionMiddleware = expressSession;
+
+    if (error) {
+      return res.send(500);
+    }
+
+    console.log('Attempting to load the session: ' + tryCount + ' of ' + maxTries + '...');
+
+    tryCount += 1;
+
+    if (req.session) {
+      return next();
+    }
+
+    if (tryCount > maxTries) {
+      return res.send(500);
+    }
+
+    sessionMiddleware(req, res, lookupSession);
+  }
+
+  lookupSession();
+});
+
+app.use('/', index);
+app.use('/news/*', index);
+app.use('/blog/*', index);
+app.use('/profile/*', index);
+
+//app.use('/newsapi', newsapi);
+//app.use('/youtube', youtube);
+app.get('/youtube/*', function (req, res) {
+  youtube(req, client, function (result) {
+    res.send(result)
+  })
+})
+app.get('/newsapi/*', function (req, res, ) {
+  console.log('/newsapi/*', req)
+  newsapi(req, client, function (result) {
+    // console.log(req)
+    console.log('/newsapi/*')
+    res.send(result)
+  }, false)
+})
 
 function update(pathName) {
   let arr = obj[pathName]
@@ -64,7 +135,7 @@ setInterval(() => {
       update('everything')
       update('topHeadLines')
       obj.date = date
-      obj.everything = [] 
+      obj.everything = []
       obj.topHeadLines = []
     })
     console.log('date cahnged', date)
@@ -81,8 +152,8 @@ setInterval(() => {
     if (result.length < obj.everything.length && result.length !== obj.everything.length) {
       update('everything')
     }
-  }) 
-   client.keys('*topHeadLines*', function (err, result) {
+  })
+  client.keys('*topHeadLines*', function (err, result) {
     //   console.log(result.length, i)
     if (result.length > obj.topHeadLines.length) {
       obj.topHeadLines = []
@@ -96,43 +167,6 @@ setInterval(() => {
     }
   })
 }, 5000)
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(responseTime());
-/*app.use('/publicbower', express.static(path.join(__dirname, 'build/modern/public/bower_components/webcomponentsjs')));
-app.use('/public', express.static(path.join(__dirname, 'build/modern/public')));
-app.use('/src', express.static(path.join(__dirname, 'build/modern/src')));*/
-
-app.use('/publicbower', express.static(path.join(__dirname, 'public/bower_components/webcomponentsjs')));
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/src', express.static(path.join(__dirname, 'src')));
-
-app.use('/', index);
-app.use('/news/*', index);
-app.use('/blog/*', index);
-app.use('/profile/*', index);
-
-//app.use('/newsapi', newsapi);
-//app.use('/youtube', youtube);
-app.get('/youtube/*', function (req, res) {
-  youtube(req, client, function (result) {
-    res.send(result)
-  })
-})
-app.get('/newsapi/*', function (req, res, ) {
-//  console.log('/newsapi/*', req)
-  newsapi(req, client, function (result) {
-    console.log('/newsapi/*')
-    res.send(result)
-  }, false)
-})
-
-//
 
 // catch 404 and forward to error handler
 
